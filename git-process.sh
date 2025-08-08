e1814059b7b2c32af38bd121e4c4b602cff61634
Yimbuh <dav.yim@gmail.com>
2025-08-08 16:53:20 +0100

    git process listed in git-process.sh

---
+++ git-process.sh
@@ -0,0 +1,10 @@
+The git process
+mkdir to creat the directory
+cd directory name to access it
+You can either creat the files manually or you can use command prompt.
+use git init to initialise versions control system whose aim is to allow collaboration, track history of changes made on a code.
+git add . to add all the changes
+once git init is done, note that the directory becomes a local repo.
+git commit -m and summary caption of the modificaiton done.
+anything committed will be seen by other members. what you don't want to move to the remote, or be seen by others, do not commit.
+This is for excercise or practice so i can capture the process of git which is a key tool at every level of the SDLC
